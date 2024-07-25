f4ddf49fafa2e2cb6d0eff85f370d522fd9f6b8b
Gilberto Willian <gilbertowillian99@gmail.com>
2024-07-25 13:07:54 -0300

    Estrutura de nagegação e cadastro - mobile

---
+++ apps/mobile/scr/screens/Principal.tsx
@@ -0,0 +1,16 @@
+import { Pressable, Text, View } from "react-native";
+
+export default function Principal({ navigation }: any) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Principal</Text>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Sumario");
+        }}
+      >
+        <Text>Ir para Sumario</Text>
+      </Pressable>
+    </View>
+  );
+}
